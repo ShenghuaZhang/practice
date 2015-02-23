56171d8be4e58c58f693a724cc9ba953f1e0869f
yili3 <yili3@CS1239-08.cs.stonybrook.edu>
2015-02-22 19:07:44 -0500

    Add a new way on validate bst

--- leetcode/src/tree/binarysearchtree/ValidateBinarySearchTree.java
+++ leetcode/src/tree/binarysearchtree/ValidateBinarySearchTree.java
@@ -7,6 +7,8 @@ import java.util.Stack;
 import tree.binarytree.TreeNode;
 
 public class ValidateBinarySearchTree {
+	/************************** 0 **************************/
+	// Using InOrder Traversal, but we do not use recursive here but using stack instead.
 	public static boolean isValidBST(TreeNode root){
 		if (root == null)	return true;
 		Stack<TreeNode> stack = new Stack<>();
@@ -28,7 +30,34 @@ public class ValidateBinarySearchTree {
 		}
 		return true;
 	}
-	
+	/************************** 1 **************************/
+	// Using the requirement we define a low and high value for each node, if not satisfy return false.
+	public boolean isValidBSTClean(TreeNode root) {
+        return helper(root, null, null);
+    }
+    private boolean helper(TreeNode root, Integer low, Integer high){
+        if (root == null)   return true;
+        return (low==null || root.val>low) && (high==null || root.val<high) 
+        		&& helper(root.left, low, root.val) && helper(root.right, root.val, high);
+    }
+    /************************** 2 **************************/
+    // Using InOrder Traversal, next node's value should bigger than the previous one.
+    TreeNode previous;
+    public boolean isValidBSTInOrder(TreeNode root) {
+        previous = null;
+        return helper(root);
+    }
+    private boolean helper(TreeNode root){
+        if (root==null) return true;
+        if (helper(root.left)){
+            if (previous == null || previous.val < root.val)   previous = root;
+            else return false;
+            return helper(root.right);
+        }
+        return false;
+    }
+    /************************** 3 **************************/
+    // Same with 2, only difference is here we use a list to save previous node value on InOrder Traversal.
 	public boolean isValidBSTbyRecursive(TreeNode root){
 		List<Integer> list = new ArrayList<>();
 		list.add(null);
@@ -41,12 +70,4 @@ public class ValidateBinarySearchTree {
 		list.set(0, root.val);
 		return left && helperRecursive(list, root.right);
 	}
-	
-	public boolean isValidBSTClean(TreeNode root) {
-        return helper(root, null, null);
-    }
-    private boolean helper(TreeNode root, Integer low, Integer high){
-        if (root == null)   return true;
-        return (low==null || root.val>low) && (high==null || root.val<high) && helper(root.left, low, root.val) && helper(root.right, root.val, high);
-    }
 }
