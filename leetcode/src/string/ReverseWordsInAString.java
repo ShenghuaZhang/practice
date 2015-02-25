3b5e6bdf3102a2ea649d6b80bc0b75e026ff8b9a
yili3 <yili3@CS1239-04.cs.stonybrook.edu>
2015-02-24 22:06:23 -0500

    reverse words

---
+++ leetcode/src/string/ReverseWordsInAString.java
@@ -0,0 +1,20 @@
+package string;
+
+public class ReverseWordsInAString {
+	public static String reverWords(String s){
+		StringBuilder reversed = new StringBuilder();
+		int j = s.length();
+		for (int i = s.length() -1; i >= 0; i--){
+			if (s.charAt(i) == ' ') j = i;
+			else if (i == 0|| s.charAt(i-1) == ' '){
+				if (reversed.length() != 0) reversed.append(' ');
+				reversed.append(s.substring(i, j));
+			}
+		}
+		return reversed.toString();
+	}
+	
+	public static void main(String[] args){
+		System.out.print(reverWords("  ksldjf   lsjdfldsj e sfd   s fdklj    "));
+	}
+}
