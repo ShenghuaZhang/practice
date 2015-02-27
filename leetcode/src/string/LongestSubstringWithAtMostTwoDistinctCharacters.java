f3d3c773625965f437a866cfde4a419a17b065e7
yili3 <yili3@CS1239-15.cs.stonybrook.edu>
2015-02-27 16:50:19 -0500

    longest substring 1

--- leetcode/src/string/LongestSubstringWithAtMostTwoDistinctCharacters.java
+++ leetcode/src/string/LongestSubstringWithAtMostTwoDistinctCharacters.java
@@ -1,5 +1,16 @@
 package string;
 
 public class LongestSubstringWithAtMostTwoDistinctCharacters {
-
+	public int lengthOfLongestSubstingTwoDistinct(String s){
+		int i=0, j=-1, maxLen=0;
+		for(int k=1; k<s.length(); k++){
+			if (s.charAt(k) == s.charAt(k-1))	continue;
+			if (j>=0 && s.charAt(j) != s.charAt(k)){
+				maxLen = Math.max(k-i, maxLen);
+				i = j+1;
+			}
+			j = k-i;
+		}
+		return Math.max(s.length()-i, maxLen);
+	}
 }
