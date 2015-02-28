b0b600a533e924132c16c56898b8d8789a6ec27f
yili3 <yili3@CS1239-15.cs.stonybrook.edu>
2015-02-28 00:30:53 -0500

    iv of best buy and sell

--- leetcode/src/dynamicProgramming/BestTimeToBuyAndSellStock.java
+++ leetcode/src/dynamicProgramming/BestTimeToBuyAndSellStock.java
@@ -11,4 +11,21 @@ public class BestTimeToBuyAndSellStock {
 		}
 		return max;
 	}
+	
+	public int maxProfitII(int[] prices){
+		if(prices==null || prices.length==0) return 0;
+		int max = 0;
+		
+		for(int i=1; i<prices.length; i++)
+			if(prices[i]-prices[i-1]>0) max += prices[i]-prices[i-1];
+		
+		return max;
+	}
+	
+	public int maxProfitIIII(int k, int[] prices){
+		if(prices==null || prices.length==0) return 0;
+		int length = prices.length;
+		
+		return -1;
+	}
 }
