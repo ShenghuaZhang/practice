fd37563f1d6f3689f4fc691f9b35efb2ee440a37
yili3 <yili3@CS1239-04.cs.stonybrook.edu>
2015-03-01 21:44:43 -0500

    some improve on populating next right pointers in each node

--- leetcode/src/tree/binarytree/PopulatingNextRightPointersInEachNode.java
+++ leetcode/src/tree/binarytree/PopulatingNextRightPointersInEachNode.java
@@ -4,61 +4,93 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class PopulatingNextRightPointersInEachNode {
-	public void connect(TreeLinkNode root){
-		if (root == null)	return;
+	// using level order traversal, but not the best, because in next solution
+	// we have space complexity only O(1) better than our O(n)
+	public void connect(TreeLinkNode root) {
+		if (root == null)
+			return;
 		Queue<TreeLinkNode> queue = new LinkedList<>();
 		queue.add(root);
 		root.next = null;
-		while(!queue.isEmpty()){
-		    int size = queue.size();
+		while (!queue.isEmpty()) {
+			int size = queue.size();
 			TreeLinkNode first = queue.poll();
-			for(int i=1;i<=size;i++){
-				if (first.left!= null){
+			for (int i = 1; i <= size; i++) {
+				if (first.left != null) {
 					queue.add(first.left);
 					queue.add(first.right);
 					first.left.next = first.right;
-				} else return;
-				if (i<size){
+				} else
+					return;
+				if (i < size) {
 					TreeLinkNode second = queue.poll();
 					first.right.next = second.left;
 					first = second;
-				}else first.right.next = null;	
+				} else
+					first.right.next = null;
 			}
 		}
 	}
-	
-	public void connectII(TreeLinkNode root){
-		if (root == null)	return;
-		TreeLinkNode lastHead = root;
-		TreeLinkNode curHead = null;
-		TreeLinkNode pre = null;
-		while(lastHead!=null){
-			TreeLinkNode lastCur = lastHead;
-			while(lastCur != null){
-				if (lastCur.left != null){
-					if (curHead == null){
-						curHead = lastCur.left;
-						pre = curHead;
-					}else {
-						pre.next = lastCur.left;
-						pre = pre.next;
-					}
-				}
-				
-				if (lastCur.right != null){
-					if (curHead == null){
-						curHead = lastCur.right;
-						pre = curHead;
-					}else {
-						pre.next = lastCur.right;
-						pre = pre.next;
-					}
-				}
-				lastCur = lastCur.next;
+
+	// better solution i think, cause we have same time complexity with previous
+	// solution which is O(n), and better space complexity which is O(1).
+	public void connectI(TreeLinkNode root) {
+		if (root == null)
+			return;
+		root.next = null;
+
+		TreeLinkNode parentHead = root, parent = parentHead;
+
+		while (parent.left != null) {
+			parent.left.next = parent.right;
+			if (parent.next == null) {
+				parent.right.next = null;
+				parentHead = parentHead.left;
+				parent = parentHead;
+			} else {
+				parent.right.next = parent.next.left;
+				parent = parent.next;
 			}
-			
-			lastHead = curHead;
-			curHead = null;
 		}
+	}
+
+	public void connectII(TreeLinkNode root) {
+		if (root == null)
+			return;
+		root.next = null;
+
+		TreeLinkNode parent = root, currentHead = null, current = null;
+
+		while (parent != null) {
+			if (parent.left != null) {
+				if (currentHead == null) {
+					currentHead = parent.left;
+					current = currentHead;
+				} else {
+					current.next = parent.left;
+					current = current.next;
+				}
+			}
+
+			if (parent.right != null) {
+				if (currentHead == null) {
+					currentHead = parent.right;
+					current = currentHead;
+				} else {
+					current.next = parent.right;
+					current = current.next;
+				}
+			}
+
+			if (parent.next == null) {
+				if (current == null)
+					return;
+				current.next = null;
+				parent = currentHead;
+				currentHead = null;
+			} else
+				parent = parent.next;
+		}
+
 	}
 }
