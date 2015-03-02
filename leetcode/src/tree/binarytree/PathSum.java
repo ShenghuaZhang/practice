defeeac838f2d74295d63ee8cb024ef2ed9e9ea7
yili3 <yili3@CS1239-04.cs.stonybrook.edu>
2015-03-01 23:08:53 -0500

    path sum improve

--- leetcode/src/tree/binarytree/PathSum.java
+++ leetcode/src/tree/binarytree/PathSum.java
@@ -4,42 +4,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PathSum {
-	public static boolean hasPathSum(TreeNode root, int sum){
-		if (root == null)	return false;
-		if (root.left == null && root.right == null && root.val == sum)	return true;
-		return hasPathSum(root.left, sum-root.val) || hasPathSum(root.right, sum-root.val);
+	public static boolean hasPathSum(TreeNode root, int sum) {
+		if (root==null)
+			return false;
+		if (root.left==null && root.right==null && root.val==sum)
+			return true;
+		return hasPathSum(root.left, sum-root.val)
+				|| hasPathSum(root.right, sum-root.val);
 	}
+
 	
-	public static List<List<Integer>> pathSum(TreeNode root, int sum){
-		List<List<Integer>>	list = new ArrayList<>();
-		if (root == null)	return list;
-		
+	/*
+	 * I should pay more attention on the mark 0 and 1.
+	 * Which remind me that I need be careful.
+	 */
+	public static List<List<Integer>> pathSum(TreeNode root, int sum) {
+		List<List<Integer>> list = new ArrayList<>();
+		if (root==null)	return list;
 		List<Integer> currentList = new ArrayList<>();
+		
 		currentList.add(root.val);
-		helper(root, sum-root.val, list, currentList);
+		helper(root, sum - root.val, list, currentList);
 		return list;
 	}
-	private static void helper(TreeNode root, int sum, List<List<Integer>> list, List<Integer> currentList){
-		if (root == null)	return;
-		if (root.left == null && root.right == null && sum == 0){
+	private static void helper(TreeNode root, int sum,
+			List<List<Integer>> list, List<Integer> currentList) {
+		if (root==null)	return;
+		if (root.left==null && root.right==null && sum==0) {
+			/********* 0 *********/
 			list.add(new ArrayList<Integer>(currentList));
 			return;
 		}
-		if (root.left != null){
+		if (root.left != null) {
 			currentList.add(root.left.val);
 			helper(root.left, sum-root.left.val, list, currentList);
+			/********* 1 *********/
 			currentList.remove(currentList.size()-1);
 		}
-		if (root.right != null){
+		if (root.right != null) {
 			currentList.add(root.right.val);
 			helper(root.right, sum-root.right.val, list, currentList);
 			currentList.remove(currentList.size()-1);
 		}
 	}
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		TreeNode root = new TreeNode();
 		root = TreeNode.testRoot();
-		System.out.print(hasPathSum(root, 8)?pathSum(root, 8):"Do not has this path.");
+		System.out.print(hasPathSum(root, 8) ? pathSum(root, 8)
+				: "Do not has this path.");
 	}
 }
