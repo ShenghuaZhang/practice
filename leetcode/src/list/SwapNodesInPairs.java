436d0888097cb820f811c2e5cf19cf9ebc0e8d45
yili3 <yili3@CS1239-04.cs.stonybrook.edu>
2015-03-06 16:41:27 -0500

    swap Nodes in pairs

---
+++ leetcode/src/list/SwapNodesInPairs.java
@@ -0,0 +1,19 @@
+package list;
+
+public class SwapNodesInPairs {
+	public ListNode swapPairs(ListNode head) {
+		ListNode dummy = new ListNode(0);
+		dummy.next = head;
+		ListNode p = head;
+		ListNode prev = dummy;
+		while (p != null && p.next != null) {
+			ListNode q = p.next, r = p.next.next;
+			prev.next = q;
+			q.next = p;
+			p.next = r;
+			prev = p;
+			p = r;
+		}
+		return dummy.next;
+	}
+}
