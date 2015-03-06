bbf1f6e260c1dda3fec2aec2e2b6ab69616eb671
yili3 <yili3@CS1239-04.cs.stonybrook.edu>
2015-03-06 00:39:41 -0500

    valid parentheses

---
+++ leetcode/src/stack/ValidParentheses.java
@@ -0,0 +1,28 @@
+package stack;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class ValidParentheses {
+	public static boolean isValid(String s){
+		final Map<Character, Character> map = new HashMap<Character, Character>(){
+			{
+				put('(', ')');
+				put('{', '}');
+				put('[', ']');
+			}
+		};
+		
+		Stack<Character> stack = new Stack<>();
+		
+		for(char c: s.toCharArray()){
+			if(map.containsKey(c))	stack.push(c);
+			else if(map.containsValue(c)){
+				if(!stack.empty() && map.get(stack.peek())==c)	stack.pop();
+				else return false;
+			}
+		}
+		return stack.empty();
+	}
+}
