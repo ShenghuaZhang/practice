fbd8101482069ff8f7f11afecb756f4eae6df6a1
yili3 <yili3@CS1239-04.cs.stonybrook.edu>
2015-03-09 10:36:55 -0400

    tiny changes on double hashing which make sure the size of hash table should be a prime number to void from crash or burn

--- leetcode/src/hash/HashDouble.java
+++ leetcode/src/hash/HashDouble.java
@@ -1,73 +1,73 @@
 package hash;
 
-
-class DataItem{
+class DataItem {
 	public int iData;
-	
-	public DataItem(int ii){
+
+	public DataItem(int ii) {
 		iData = ii;
 	}
 }
 
-class HashTable{
+class HashTable {
 	private final int CONSTANT = 6;
 	DataItem[] hashArray;
 	int arraySize;
 	DataItem nonItem;
-	
-	HashTable(int size){
+
+	HashTable(int size) {
 		arraySize = size;
 		hashArray = new DataItem[arraySize];
 		nonItem = new DataItem(-1);
 	}
-	
-	public void displayTable(){
+
+	public void displayTable() {
 		System.out.println("Table: ");
-		for(int j=0; j<arraySize; j++){
-			if(hashArray[j] != null)
-				System.out.print(hashArray[j].iData+" ");
+		for (int j = 0; j < arraySize; j++) {
+			if (hashArray[j] != null)
+				System.out.print(hashArray[j].iData + " ");
 			else
 				System.out.print("** ");
 		}
 		System.out.println("");
 	}
-	
-	public int hashFunc1(int key){
+
+	public int hashFunc1(int key) {
 		return key % arraySize;
 	}
-	
-	public int hashFunc2(int key){
-		return CONSTANT-(key%CONSTANT);
+
+	public int hashFunc2(int key) {
+		return CONSTANT - (key % CONSTANT);
 	}
-	
-	public void insert(int key){
+
+	public void insert(int key) {
 		int count = 0;
 		int location = hashFunc1(key);
 		int stepSize = hashFunc2(key);
 		DataItem item = new DataItem(key);
-		
-		while(hashArray[location]!= null && hashArray[location].iData!=-1 && count<arraySize){
+
+		while (hashArray[location] != null && hashArray[location].iData != -1
+				&& count < arraySize) {
 			count++;
-			location = (location+stepSize) % arraySize;
+			location = (location + stepSize) % arraySize;
 		}
 		// TODO why cannot: hashArray[location].iData = key;
 		hashArray[location] = item;
 	}
-	
-	public DataItem find(int key){
+
+	public DataItem find(int key) {
 		int location = hashFunc1(key);
 		int stepSize = hashFunc2(key);
-		while(hashArray[location]!=null){
-			if(hashArray[location].iData==key)
+		while (hashArray[location] != null) {
+			if (hashArray[location].iData == key)
 				return hashArray[location];
-			location = (location+stepSize) % arraySize;
+			location = (location + stepSize) % arraySize;
 		}
-		return null;	
+		return null;
 	}
-	
-	public boolean delete(int key){
+
+	public boolean delete(int key) {
 		DataItem item = find(key);
-		if(item!=null){
+		if (item != null) {
 			item = nonItem;
 			return true;
 		}
@@ -75,17 +75,20 @@ class HashTable{
 	}
 }
 
-class HashDouble{
-	public static void main(String[] args){
-		int aKey;
-		int hashSize=10;
+class HashDouble {
+	public static void main(String[] args) {
+		/**
+		 * Should be a Prime Number, makes it impossible for any number to
+		 * divide it evenly, so the probe sequence will eventually check every
+		 * cell.
+		 */
+		int hashSize = 11;
 		HashTable hashTable = new HashTable(hashSize);
-		
-		for(int j=0; j<4; j++){
-			aKey = (int)(java.lang.Math.random()*2*hashSize);
-			hashTable.insert(aKey);
+
+		for (int j = 0; j < hashSize/2; j++){
+			hashTable.insert((int) (java.lang.Math.random() * 2 * hashSize));
+			hashTable.displayTable();
 		}
-		
-		hashTable.displayTable();
+
 	}
 }
