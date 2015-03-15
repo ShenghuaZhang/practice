fb2be4c8cfc9f141d4c0448818a470df1e4957f2
yili3 <yili3@CS1239-04.cs.stonybrook.edu>
2015-03-15 11:31:16 -0400

    some test demo on array and arraylist

---
+++ leetcode/src/test/ArrayDemo.java
@@ -0,0 +1,15 @@
+package test;
+
+import java.util.Arrays;
+
+public class ArrayDemo {
+	public static void main(String args[]){
+		/************* print *************/
+		int[] A = {8,12,3,52,6,7};
+		Arrays.sort(A);
+		System.err.println(A);
+		System.out.println(Arrays.toString(A));
+		
+		
+	}
+}
--- leetcode/src/test/ArrayListInitializeDemo.java
+++ leetcode/src/test/ArrayListInitializeDemo.java
@@ -11,7 +11,7 @@ import java.util.List;
  * 
  * @author carllee1991
  */
-public class ArrayListDemo {
+public class ArrayListInitializeDemo {
 
 	public static void main(String args[]) {
 
