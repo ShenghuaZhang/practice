df87e74a0f84db1b0a866cf870a11bf2fc80bc76
yili3 <yili3@CS1239-03.cs.stonybrook.edu>
2015-03-18 16:25:02 -0400

    insertion sort

--- practice/src/sort/SelectionSort.java
+++ practice/src/sort/SelectionSort.java
@@ -1,5 +1,12 @@
 package sort;
 
 public class SelectionSort {
-	
+	public void selectionSort(int[] A){
+		for(int i=0; i<A.length-1; i++){
+			int min = i;
+			for(int j=i+1; j<A.length; j++)
+				if(A[j]<A[min])	min=j;
+			BubbleSort.swap(A, i, min);
+		}
+	}
 }
