5e9b3108c0bec317d9d8220803e97548f740c3a8
yili3 <yili3@CS1239-14.cs.stonybrook.edu>
2015-03-21 15:47:50 -0400

    bb

---
+++ leetcode/src/list/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,16 @@
+package list;
+/**
+ * https://leetcode.com/problems/intersection-of-two-linked-lists/
+ * 
+ * Write a program to find the node
+ * at which the intersection of two singly linked lists begins.
+ * @author yili3
+ *
+ */
+public class IntersectionOfTwoLinkedLists {
+	public static ListNode getIntersectionNode(ListNode headA, ListNode headB){
+		
+		
+		return null;
+	}
+}
