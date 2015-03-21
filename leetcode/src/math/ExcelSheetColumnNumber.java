2d465e3291cec24205f96594a01427209c5686a2
yili3 <yili3@CS1239-14.cs.stonybrook.edu>
2015-03-21 14:28:10 -0400

    excel sheet column number

---
+++ leetcode/src/math/ExcelSheetColumnNumber.java
@@ -0,0 +1,24 @@
+package math;
+
+/**
+ * https://leetcode.com/problems/excel-sheet-column-number/
+ * 
+ * Related to question Excel Sheet Column Title.
+ * Given a column title as appear in an Excel sheet,
+ * return its corresponding column number.
+ * 
+ * @author yili3
+ *
+ */
+public class ExcelSheetColumnNumber {
+	public static int titleToNumber(String s){
+		int num = 0;
+		for(char c:s.toCharArray())
+			num = num*26+(c-64);
+		return num;
+	}
+	
+	public static void main(String[] args){
+		System.out.print(titleToNumber("AAA"));
+	}
+}
