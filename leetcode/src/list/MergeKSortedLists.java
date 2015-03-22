ff0a34c5233ae24869ee616fab4849d640ff38b9
yili3 <yili3@CS1239-13.cs.stonybrook.edu>
2015-03-22 10:44:42 -0400

    still working on the algorithm using heap

--- leetcode/src/list/MergeKSortedLists.java
+++ leetcode/src/list/MergeKSortedLists.java
@@ -1,9 +1,36 @@
 package list;
 
+/**
+ * https://leetcode.com/problems/merge-k-sorted-lists/
+ * 
+ * Merge k sorted linked lists and return it as one sorted list.
+ * Analyze and describe its complexity.
+ * 
+ */
 import java.util.List;
+
 public class MergeKSortedLists {
-	public ListNode mergeKLists(List<ListNode> lists){
-		// TODO merge K lists
-		return null;
+	/*
+	 * Using merge sorting
+	 * Time complexity: O(n*k*logk) k is the number of list
+	 * Space: O(logk)
+	 */
+	public static ListNode mergeKLists(List<ListNode> lists) {
+		if (lists == null || lists.size() == 0)
+			return null;
+		return helper(lists, 0, lists.size() - 1);
 	}
+	private static ListNode helper(List<ListNode> lists, int start, int end) {
+		if (start < end) {
+			int middle = (start + end) / 2;
+			return MergeTwoSortedLists.mergeTwoLists(
+					helper(lists, start, middle),
+					helper(lists, middle + 1, end));
+		}
+		return lists.get(start);
+	}
+	
+	/*
+	 * Using heap
+	 */
 }
