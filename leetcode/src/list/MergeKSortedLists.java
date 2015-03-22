d5603e303cb7ce474fd45d40008409407781385c
yili3 <yili3@CS1239-13.cs.stonybrook.edu>
2015-03-22 10:46:52 -0400

    heap

--- leetcode/src/list/MergeKSortedLists.java
+++ leetcode/src/list/MergeKSortedLists.java
@@ -7,7 +7,9 @@ package list;
  * Analyze and describe its complexity.
  * 
  */
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class MergeKSortedLists {
 	/*
@@ -20,6 +22,7 @@ public class MergeKSortedLists {
 			return null;
 		return helper(lists, 0, lists.size() - 1);
 	}
+
 	private static ListNode helper(List<ListNode> lists, int start, int end) {
 		if (start < end) {
 			int middle = (start + end) / 2;
@@ -29,8 +32,37 @@ public class MergeKSortedLists {
 		}
 		return lists.get(start);
 	}
-	
+
 	/*
 	 * Using heap
+	 * Time complexity: O(n*k*logk) k is the number of list
+	 * Space: O(k)
 	 */
+	public ListNode mergeKListsHeap(List<ListNode> lists) {
+		PriorityQueue<ListNode> heap = new PriorityQueue<ListNode>(10,
+				new Comparator<ListNode>() {
+					@Override
+					public int compare(ListNode n1, ListNode n2) {
+						return n1.val - n2.val;
+					}
+				});
+		for (int i = 0; i < lists.size(); i++) {
+			ListNode node = lists.get(i);
+			if (node != null)	heap.offer(node);
+
+		}
+		ListNode head = null;
+		ListNode pre = head;
+		while (heap.size() > 0) {
+			ListNode cur = heap.poll();
+			if (head == null) {
+				head = cur;
+				pre = head;
+			} else	pre.next = cur;
+			pre = cur;
+			if (cur.next != null)	heap.offer(cur.next);
+		}
+		
+		return head;
+	}
 }
