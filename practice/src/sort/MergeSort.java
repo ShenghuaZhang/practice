1b381515b65e7f5eba21edda752bf9f66e21d76b
yili3 <yili3@CS1239-04.cs.stonybrook.edu>
2015-03-28 14:44:57 -0400

    merge and primitive and wrappers

--- practice/src/sort/MergeSort.java
+++ practice/src/sort/MergeSort.java
@@ -1,5 +1,31 @@
 package sort;
 
-public class MergeSort {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class MergeSort {
+	protected static Integer[] merge(int[] A, int[] B){
+		int length1 = 0, length2 = 0;
+		List<Integer> list = new ArrayList<>();
+		
+		while(length1<A.length && length2<B.length){
+			if(A[length1]>B[length2])	list.add(B[length2++]);
+			else list.add(A[length1++]);
+		}
+		
+		while(length1<A.length)	list.add(A[length1++]);
+		while(length2<B.length)	list.add(B[length2++]);
+		
+		Integer[] C = list.toArray(new Integer[list.size()]);
+		System.out.println(Arrays.toString(C));
+		return C;
+	}
+	
+	public static void main(String[] args){
+		int[] A = {12,3};
+		int[] B = {11,20,51};
+		QuickSort.quickSort(A, B);
+		merge(A, B);
+	}
 }
--- practice/src/test/PrimitiveAndWrappers.java
+++ practice/src/test/PrimitiveAndWrappers.java
@@ -21,6 +21,8 @@ public class PrimitiveAndWrappers {
 			vec[i] = i;
 		long end_mem = freemem();
 		long n = (start_mem - end_mem) / N;
+
+		System.out.println("********* test01 *********");
 		System.out.println("bytes per element = " + n);
 	}
 
@@ -32,6 +34,9 @@ public class PrimitiveAndWrappers {
 			vec[i] = new Integer(i);
 		long end_mem = freemem();
 		long n = (start_mem - end_mem) / N;
+
+		System.out.println();
+		System.out.println("********* test02 *********");
 		System.out.println("bytes per element = " + n);
 	}
 
@@ -41,9 +46,50 @@ public class PrimitiveAndWrappers {
 	// technique for determining memory use per element should be used
 	// cautiously.
 
+	public static void test03() {
+		Integer a = Integer.valueOf(100);
+		Integer b = 100;
+
+		Integer c = 200;
+		Integer d = 200;
+
+		System.out.println();
+		System.out.println();
+		System.out.println("********* test03 *********");
+		System.out.println(a == b); // 1
+		System.out.println(a == 100); // 2
+
+		System.out.println(c == d); // 3
+		System.out.println(c == 200); // 4
+	}
+
+	public static void test04() {
+		Integer m = new Integer(5);
+		Integer n = new Integer(5);
+		System.out.println();
+		System.out.println("********* test04 *********");
+		System.out.println(m == n);
+		m = m - 1;
+		n = n - 1;
+		System.out.println(m == n);
+	}
+
+	public static void test05() {
+		Integer i = new Integer(0);
+		Integer j = new Integer(0);
+		
+		System.out.println();
+		System.out.println("********* test04 *********");
+		if (i <= j & i >= j & i != j)
+			System.out.println("0000");
+	}
+
 	public static void main(String args[]) {
 		test01();
 		test02();
+		test03();
+		test04();
+		test05();
 	}
 
 }
