a84fa58d8e20682088ffc8d31efca4028479d083
yili3 <yili3@CS1239-04.cs.stonybrook.edu>
2015-03-30 18:43:55 -0400

    maximum gap Radix sort

--- practice/src/sort/RadixSort.java
+++ practice/src/sort/RadixSort.java
@@ -6,12 +6,12 @@ public class RadixSort {
 	public static void radixSort(int[] A){
 		int n=1, k=0, length=A.length;
 		int[][] bucket = new int[10][length];
-		int[] order= new int[length];
+		int[] order= new int[10];
 		
 		int max = A[0];
 		for(int num:A)	max=Math.max(max, num);
 		
-		while(n<max){
+		while(n<=max){
 			for(int num:A){
 				int digit = (num/n) % 10;
 				bucket[digit][order[digit]] = num;
@@ -28,7 +28,7 @@ public class RadixSort {
 	}
 	
 	public static void main(String[] args){
-		int[] A = {12,13,14,45,65,76,36,45,90,87,97,56,76,1,4,6,8,9};
+		int[] A = {1, 100000};
 		radixSort(A);
 		System.out.println(Arrays.toString(A));
 	}
