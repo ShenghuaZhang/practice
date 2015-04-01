5985c38f14fc941c43cd2a7332df205d64fab913
yili3 <yili3@CS1239-02.cs.stonybrook.edu>
2015-04-01 15:23:44 -0400

    kmp

--- practice/src/string/KMP.java
+++ practice/src/string/KMP.java
@@ -1,67 +1,56 @@
 package string;
 
 public class KMP {
-	static int[] getPartialMatchTable(String s){
-		int length = s.length();
-		int[] table = new int[length];
-		table[0] = 0;
+	private String pattern;
+	private int[] table;
+	
+	public KMP(String pattern){
+		this.pattern = pattern;
+
+		int length = pattern.length();
+		table = new int[length];
 		
-		int start = 0, i = 1, previous=0;
-		boolean flag = false;
-		while(i<length){
-			if(s.charAt(start) == s.charAt(i)){
-				table[i] = previous+1;
-				previous = table[i];
-				start++;
-				i++;
-				flag=false;
+		int pre = 0, suffix = 0, value=0;
+		boolean fromBegin = true;
+		while(suffix < length){
+			if(suffix == 0)	table[suffix++] = 0;
+			if(pattern.charAt(pre) == pattern.charAt(suffix)){
+				table[suffix++] = ++value;
+				pre++;
+				fromBegin=false;
 			}else{
-				previous = 0;
-				if(start+1 == i){
-					table[i] = 0;	
-					start=0;
-					i++;
-					flag=false;
-				}else if(!flag){
-					previous = 0;
-					start=0;
-					flag = true;
-				}else{
-					previous = 0;
-					start++;
-				}
+				value = 0;
+				if(pre+1 == suffix){
+					table[suffix++] = 0;	
+					pre=0;
+				}else if(!fromBegin){
+					pre=0;
+					fromBegin = true;
+				}else	pre++;
 			}
 		}
-		return table;
+		
+		for(int i=table.length-1; i>=0; i--)
+			table[i] = (i==0)?-1:table[i-1];
 	}
 	
-	static int[] getNext(String s){
-		int[] table = new int[s.length()];
-		table = getPartialMatchTable(s);
-
-		for(int i=table.length-1; i>0; i--)
-			table[i] = table[i-1];
-		table[0] = -1;
-		return table;
-	}
-	
-	static boolean kmp(String target, String s){
-		if(target.length()<s.length())	return false;
-		int[] next = getNext(s);
+	static boolean kmp(String text, String pattern){
+		if(text.length()<pattern.length())	return false;
+		KMP kmp = new KMP(pattern);
 		
 		int i=0, j=0;
-		while(i<target.length() && j<s.length()){
-			if(j==-1 || target.charAt(i)==s.charAt(j)){
+		while(i<text.length() && j<pattern.length()){
+			if(j==-1 || text.charAt(i)==pattern.charAt(j)){
 				i++;
 				j++;
-			}else	j = next[j];
+			}else	j = kmp.table[j];
 		}
 		
-		if(j==s.length())	return true;
-		else return false;
+		if(j==pattern.length())	return true;
+		return false;
 	}
 	
 	public static void main(String[] args){
-		System.out.println(kmp("ababsabs", "xabs"));
+		System.out.println(kmp("dabcabdabcxxxasdfsaf", "dabcabsddabcxxx"));
 	}
 }
