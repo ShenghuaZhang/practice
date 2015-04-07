07d8ae4e79f443dd886b46ff80cc7cf212a4e520
yili3 <yili3@CS1239-14.cs.stonybrook.edu>
2015-04-06 21:17:53 -0400

    tomorrow

---
+++ leetcode/src/dynamicProgramming/DistinctSubsequences.java
@@ -0,0 +1,20 @@
+package dynamicProgramming;
+
+/**
+ * https://leetcode.com/problems/distinct-subsequences/
+ * 
+ * Given a string S and a string T, count the number of distinct subsequences of
+ * T in S.
+ * 
+ * Here is an example:
+ * S = "rabbbit", T = "rabbit"
+ * Return 3.
+ * 
+ * @author yili3
+ *
+ */
+public class DistinctSubsequences {
+	public int numDistinct(String S, String T){
+		
+	}
+}
