eeeafb43740a864192cfbbd6030627044537382c
yili3 <yili3@CS1239-13.cs.stonybrook.edu>
2015-04-09 17:20:30 -0400

    discontinuous matches google

---
+++ practice/src/google/DiscontinuousMatches.java
@@ -0,0 +1,14 @@
+package google;
+/**
+ * http://www.careercup.com/question?id=5757597581836288
+ * 
+ * Question: Given two strings, find number of discontinuous matches.
+ * Example: “cat”, “catapult”
+ * Output: 3   => “CATapult”, “CatApulT”, “CAtapulT”
+ * 
+ * @author yili3
+ * 
+ */
+public class DiscontinuousMatches {
+
+}
