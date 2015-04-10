cb22c995c1af7fb814846599558a251a9d6748bc
yili3 <yili3@CS1239-13.cs.stonybrook.edu>
2015-04-09 20:14:43 -0400

    discontinuous matches

--- practice/src/google/DiscontinuousMatches.java
+++ practice/src/google/DiscontinuousMatches.java
@@ -10,5 +10,24 @@ package google;
  * 
  */
 public class DiscontinuousMatches {
-
+	static int getDiscontinuousMatches(String pattern, String text){
+		int[][] dp = new int[pattern.length()+1][1+text.length()];
+		for(int i=0; i<pattern.length()+1; i++)	dp[i][0] = 0;
+		for(int i=0; i<text.length()+1; i++)	dp[0][i] = 1;
+		
+		for(int i=0; i<pattern.length(); i++){
+			for(int j=0; j<text.length(); j++){
+				if(pattern.charAt(i)==text.charAt(j))
+					dp[i+1][j+1] = dp[i][j] + dp[i+1][j];
+				else
+					dp[i+1][j+1] = dp[i+1][j]; 
+			}
+		}
+		
+		return dp[pattern.length()][text.length()];
+	}
+	public static void main(String[] args){
+		String pattern = "cat", text="catapult";
+		System.out.print(getDiscontinuousMatches(pattern, text));
+	}
 }
