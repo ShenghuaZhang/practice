07a57079018472e6a15680d3feecd1911082e9b2
carllee1991 <carllee1991@gmail.com>
2015-04-12 02:55:24 -0400

    integer to roman

--- leetcode/src/math/RomanToInteger.java
+++ leetcode/src/math/RomanToInteger.java
@@ -8,12 +8,12 @@ import java.util.Map;
  * 
  * Given a ROMAN numeral, convert it to an integer.
  * Input is guaranteed to be within the range from 1 to 3999.
+ * N: 5000 M: 1000 D: 500 C: 100 L: 50 X: 10 V:5 I:1
  * 
  * @author yili3
- *
+ * #IntegerToRoman
  */
 public class RomanToInteger {
-	// TODO: Know the way to initialize the HashMap #EvaluateReversePolishNotation
 	private static final Map<Character, Integer> map = new HashMap<Character, Integer>(){
 		private static final long serialVersionUID = 1L;
 		{
@@ -41,5 +41,9 @@ public class RomanToInteger {
 		}
 		
 		return result;
+	}
+	
+	public static void main(String[] args){
+		System.out.print(romanToInt("CM"));
 	}
 }
