0e7628a57a58aaa6721851bd55dd98ead74d593f
yili3 <yili3@CS1239-04.cs.stonybrook.edu>
2015-04-28 00:38:39 -0400

    fraction to recuring decimal

--- leetcode/src/math/CountPrimes.java
+++ leetcode/src/math/CountPrimes.java
@@ -28,10 +28,10 @@ public class CountPrimes {
 	    }
 	    return count;
 	}
-	
+
 	public int countPrimes(int n) {
 		int cnt = 0;
-		boolean isPrime[] = new boolean[n];
+		boolean isPrime[] = new boolean[n + 1];
 		for (int i = 2; i < n; i++)
 			isPrime[i] = true;
 		for (int i = 2; i * i < n; i++) {
---
+++ leetcode/src/math/FractionToRecuringDecimal.java
@@ -0,0 +1,45 @@
+package math;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 167
+ * https://leetcode.com/problems/fraction-to-recurring-decimal/
+ * 
+ * Given two integers representing the numerator and denominator of a fraction,
+ * return the fraction in string format.
+ * If the fractional part is repeating, enclose the repeating part in parentheses.
+ * 
+ * For example,
+ * Given numerator = 1, denominator = 2, return "0.5".
+ * Given numerator = 2, denominator = 1, return "2".
+ * Given numerator = 2, denominator = 3, return "0.(6)".
+ * 
+ * @author yili3
+ *
+ */
+public class FractionToRecuringDecimal {
+	 public String fractionToDecimal(int numerator, int denominator) {
+		 StringBuilder sb = new StringBuilder();
+		 String sign = (numerator<0==denominator<0 || numerator==0)?"":"-";
+		 long num = Math.abs((long)numerator);
+		 long den = Math.abs((long)denominator);
+		 sb.append(sign);
+		 sb.append(num/den);
+		 
+		 long remainder = num%den;
+		 if(remainder==0)	return sb.toString();
+		 sb.append(".");
+		 Map<Long, Integer> map = new HashMap<>();
+		 while(!map.containsKey(remainder)){
+			 map.put(remainder,  sb.length());
+			 sb.append(10*remainder/den);
+			 remainder = 10 * remainder % den;
+		 }
+		 int index = map.get(remainder);
+		 sb.insert(index, "(");
+		 sb.append(")");
+		 return sb.toString().replace("(0)", "");
+	  }
+}
