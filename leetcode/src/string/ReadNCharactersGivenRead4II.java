5c41ffcef465f894f4f83515956458b5dd648a49
yili3 <yili3@CS1239-02.cs.stonybrook.edu>
2015-04-29 03:52:11 -0400

    s

--- leetcode/src/string/ReadNCharactersGivenRead4II.java
+++ leetcode/src/string/ReadNCharactersGivenRead4II.java
@@ -1,16 +1,17 @@
 package string;
-
-public class ReadNCharactersGivenRead4CallMultipleTimes extends ReadNCharactersGiveRead4{
+/**
+ * 158
+ * https://leetcode.com/problems/read-n-characters-given-read4-ii-call-multiple-times/
+ * The read function may be called multiple times.
+ * 
+ * @author yili3
+ *
+ * #ReadNCharactersGiveRead4
+ */
+public class ReadNCharactersGivenRead4II extends ReadNCharactersGiveRead4{
 	private char[] buffer = new char[4];
 	int offset = 0, bufsize = 0;
 
-	/**
-	 * https://leetcode.com/problems/read-n-characters-given-read4-ii-call-multiple-times/
-	 *  
-	 * @param buf 	Destination buffer
-	 * @param n  	Maximum number of characters to read
-	 * @return 		The number of characters read
-	 */
 	public int readIII(char[] buf, int n) {
 		int readBytes = 0;
 		boolean eof = false;
