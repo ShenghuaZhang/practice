8fded5bcc2afa3d086be608ff4f5942c0a881dfb
yili3 <yili3@CS1239-02.cs.stonybrook.edu>
2015-05-02 00:52:55 -0400

    jump game

--- leetcode/src/string/PalindromePartitioningII.java
+++ leetcode/src/string/PalindromePartitioningII.java
@@ -2,7 +2,9 @@ package string;
 /**
  * 132
  * https://leetcode.com/problems/palindrome-partitioning-ii/
- * Given a string s, partition s such that every substring of the partition is a palindrome.
+ * 
+ * Given a string s,
+ * partition s such that every substring of the partition is a palindrome.
  * Return the minimum cuts needed for a palindrome partitioning of s.
  * 
  * For example, given s = "aab",
@@ -10,11 +12,32 @@ package string;
  * 
  * @author yili3
  * 
- * #PalindromePartitioning #WordBreak
+ * #PalindromePartitioning #WordBreak #WordBreakII
  *
  */
 public class PalindromePartitioningII {
 	public int minCut(String s){
+		boolean[][] palinDict = findAllPalin(s);
 		
+		int[] res = new int[s.length()];
+		for(int i=0; i<s.length(); i++){
+			res[i] = i;
+			for(int j=0; j<=i; j++){
+				if(palinDict[j][i])
+					res[i] = Math.min(res[i], res[j]+1);
+			}
+		}
+		return res[s.length()-1];
+	}
+	private boolean[][] findAllPalin(String s){
+		boolean[][] palinDict = new boolean[s.length()][s.length()];
+		
+		for(int i=0; i<s.length(); i++){
+			for(int j=0; j<=i; j++){
+				if(s.charAt(i)==s.charAt(j)&&(i-j<2||palinDict[j+1][i-1]))
+					palinDict[j][i] = true;
+			}
+		}
+		return palinDict;
 	}
 }
