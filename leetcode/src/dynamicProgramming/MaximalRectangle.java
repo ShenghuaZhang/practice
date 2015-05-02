1357431f89a433db8f231a6d5b169266c0ffbd8f
yili3 <yili3@CS1239-02.cs.stonybrook.edu>
2015-05-02 18:55:42 -0400

    maximal rectangle

---
+++ leetcode/src/dynamicProgramming/MaximalRectangle.java
@@ -0,0 +1,16 @@
+package dynamicProgramming;
+/**
+ * TODO 85
+ * https://leetcode.com/problems/maximal-rectangle/
+ * 
+ * Given a 2D binary matrix filled with 0's and 1's,
+ * find the largest rectangle containing all ones and return its area.
+ * 
+ * @author yili3
+ *
+ */
+public class MaximalRectangle {
+	public int maximalRectangle(char[][] matrix){
+		
+	}
+}
