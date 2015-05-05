392205f82789d339b01ec0b4b9cc738ee7baf9fb
yili3 <yili3@CS1239-02.cs.stonybrook.edu>
2015-05-04 20:43:54 -0400

    rewrite merge k sorted list

--- leetcode/src/list/MergeKSortedLists.java
+++ leetcode/src/list/MergeKSortedLists.java
@@ -1,6 +1,7 @@
 package list;
 
 /**
+ * 23
  * https://leetcode.com/problems/merge-k-sorted-lists/
  * 
  * Merge k sorted linked lists and return it as one sorted list.
@@ -8,7 +9,6 @@ package list;
  * 
  */
 import java.util.Comparator;
-import java.util.List;
 import java.util.PriorityQueue;
 
 public class MergeKSortedLists {
@@ -17,52 +17,64 @@ public class MergeKSortedLists {
 	 * Time complexity: O(n*k*logk) k is the number of list
 	 * Space: O(logk)
 	 */
-	public static ListNode mergeKLists(List<ListNode> lists) {
-		if (lists == null || lists.size() == 0)
-			return null;
-		return helper(lists, 0, lists.size()-1);
-	}
-
-	private static ListNode helper(List<ListNode> lists, int start, int end) {
-		if (start < end) {
-			int middle = (start + end) / 2;
-			return MergeTwoSortedLists.mergeTwoLists(
-					helper(lists, start, middle),
-					helper(lists, middle + 1, end));
-		}
-		return lists.get(start);
-	}
+	public ListNode mergeKLists(ListNode[] lists) {
+        if(lists==null || lists.length==0)  return null;
+       	return helper(lists, 0, lists.length-1);
+    }
+    private ListNode helper(ListNode[] lists, int start, int end){
+        if(start<end){
+            int middle = (start+end)/2;
+            return mergeList(helper(lists, start, middle), helper(lists, middle+1, end));
+        }
+        return lists[start];
+    }
+    private ListNode mergeList(ListNode first, ListNode second){
+        ListNode dummyHead = new ListNode(0), previous = dummyHead;
+        while(first!=null && second!=null){
+            if(first.val>second.val){
+                previous.next = second;
+                second=second.next;
+            }else{
+                previous.next = first;
+                first= first.next;
+            }
+            previous=previous.next;
+        }
+        if(first!=null) previous.next =first;
+        if(second!=null)    previous.next =second;
+        
+        return dummyHead.next;
+    }
 
 	/*
 	 * Using heap
 	 * Time complexity: O(n*k*logk) k is the number of list
 	 * Space: O(k)
+	 * PriorityQueue(int initialCapacity, Comparator<? super E> comparator)
+	 * Creates a PriorityQueue with the specified initial capacity that orders
+	 * its elements according to the specified comparator.
 	 */
-	public ListNode mergeKListsHeap(List<ListNode> lists) {
-		PriorityQueue<ListNode> heap = new PriorityQueue<ListNode>(10,
-			new Comparator<ListNode>() {
-				@Override
-				public int compare(ListNode n1, ListNode n2) {
-					return n1.val - n2.val;
-				}
-		});
-		for (int i = 0; i < lists.size(); i++) {
-			ListNode node = lists.get(i);
-			if (node != null)	heap.offer(node);
-
-		}
-		ListNode head = null;
-		ListNode pre = head;
-		while (heap.size() > 0) {
-			ListNode cur = heap.poll();
-			if (head == null) {
-				head = cur;
-				pre = head;
-			} else	pre.next = cur;
-			pre = cur;
-			if (cur.next != null)	heap.offer(cur.next);
-		}
+	public ListNode mergeKListsHeap(ListNode[] lists) {
+		PriorityQueue<ListNode> heap = new PriorityQueue<ListNode>(lists.length,
+				new Comparator<ListNode>() {
+					public int compare(ListNode n1, ListNode n2) {
+						return n1.val - n2.val;
+					}
+				});
 		
-		return head;
+		for (int i = 0; i < lists.length; i++)
+			if (lists[i] != null)
+				heap.offer(lists[i]);
+		
+		ListNode dummyHead = new ListNode(0), previous = dummyHead;
+		while (heap.size() > 0) {
+			ListNode current = heap.poll();
+			if (current.next != null)
+				heap.offer(current.next);
+			previous.next = current;
+			previous = current;
+		}
+
+		return dummyHead.next;
 	}
 }
