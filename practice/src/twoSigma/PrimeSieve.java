3187510b2a2c8a3d8d2029e84e18f7ebcdb32213
carllee1991 <carllee1991@gmail.com>
2015-05-11 03:05:10 -0400

    prime sieve

--- practice/src/twoSigma/PrimeSieve.java
+++ practice/src/twoSigma/PrimeSieve.java
@@ -17,13 +17,10 @@ public class PrimeSieve {
 		boolean[] isPrime = new boolean[N+1];
 		for(int i=2; i<=N; i++)	isPrime[i] = true;
 		
-		for(int i=2; i*i<=N; i++){
-			if(isPrime[i]){
-				for(int j=i; i*j<=N; j++){
+		for(int i=2; i*i<=N; i++)
+			if(isPrime[i])
+				for(int j=i; i*j<=N; j++)
 					isPrime[i*j] = false;
-				}
-			}
-		}
 		
 		for(int i=2; i<=N; i++)
 			if(isPrime[i]) System.out.print(i+" ");
