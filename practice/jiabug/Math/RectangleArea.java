af0f0ed80ff78f70242bf1545a01099ff14be86e
jiabug <jiakangze@gmail.com>
2015-06-19 14:19:23 -0400

    a better method for the rectangle area

--- practice/jiabug/Math/RectangleArea.java
+++ practice/jiabug/Math/RectangleArea.java
@@ -18,6 +18,23 @@ public class RectangleArea {
 		// TODO Auto-generated method stub
 
 	}
+	
+	public static int computeArea_better(int A, int B, int C, int D, int E, int F, int G, int H) {
+		int area_rectangle1 = (C - A) * (D - B);
+		int area_rectangle2 = (G - E) * (H - F);
+		
+		int left = Math.max(A, E);
+		int right = Math.min(G, C);
+		int top = Math.min(D, H);
+		int bottom = Math.max(B, F);
+		
+		int overlap = 0;
+		
+		if (left < right && bottom < top) {
+			overlap = (right - left) * (top - bottom);
+		}
+		return area_rectangle1 + area_rectangle2 - overlap;
+	}
 	public static int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
 		int area_overlap = 0;
 		int width = 0;
