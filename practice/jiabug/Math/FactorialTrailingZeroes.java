387adf9f4f4929b07018fb210b3daaf572e5440f
jiabug <jiakangze@gmail.com>
2015-06-21 21:43:46 -0400

    begin Factorial Trailing Zeroes (172, leetcode)

---
+++ practice/jiabug/Math/FactorialTrailingZeroes.java
@@ -0,0 +1,31 @@
+package Math;
+
+
+/**
+ * 172
+ * 
+ * https://leetcode.com/problems/factorial-trailing-zeroes/
+ * 
+ * Given an integer n, return the number of trailing zeroes in n!.
+ * Note: Your solution should be in logarithmic time complexity.
+ * 
+ * 
+ * @author jiakangze
+ *
+ */
+
+public class FactorialTrailingZeroes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+    public static int trailingZeroes(int n) {
+        int res = 0;
+        
+        while () {
+        	int multi = n / 10;
+        	int remainder = n % 10;
+        }
+    }
+}
