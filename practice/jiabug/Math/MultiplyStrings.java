f432380e628734bdcc3c8ed72b125fbd19e96296
jiabug <jiakangze@gmail.com>
2015-06-28 11:17:43 -0400

    begin multiply strings

---
+++ practice/jiabug/Math/MultiplyStrings.java
@@ -0,0 +1,27 @@
+package Math;
+
+/**
+ * 43
+ * https://leetcode.com/problems/multiply-strings/
+ * 
+ * Given two numbers represented as strings, return multiplication of the numbers as a string.
+ * Note: The numbers can be arbitrarily large and are non-negative.
+ * 
+ * 
+ * @author jiakangze
+ *
+ */
+
+
+public class MultiplyStrings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+    public static String multiply(String num1, String num2) {
+        
+    }
+
+}
