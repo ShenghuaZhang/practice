33e56477526a2297fe44c14282927f151248aa1e
kajia <kajia@CS1239-08.cs.stonybrook.edu>
2015-06-28 00:50:42 -0400

    begin permutation sequence

---
+++ practice/jiabug/Math/PermutationSequence.java
@@ -0,0 +1,37 @@
+package Math;
+
+/**
+ * 60
+ * https://leetcode.com/problems/permutation-sequence/
+ * 
+ * 
+ * The set [1,2,3,…,n] contains a total of n! unique permutations.
+ * By listing and labeling all of the permutations in order,
+ * We get the following sequence (ie, for n = 3):
+ * 
+ * 1. "123"
+ * 2. "132"
+ * 3. "213"
+ * 4. "231"
+ * 5. "312"
+ * 6. "321"
+ * 
+ * Given n and k, return the kth permutation sequence.
+ * Note: Given n will be between 1 and 9 inclusive.
+ * 
+ * @author kajia
+ *
+ */
+
+
+public class PermutationSequence {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static String getPermutation(int n, int k) {
+		
+	}
+
+}
