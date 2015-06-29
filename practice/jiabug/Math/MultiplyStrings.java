dd2e6860180ea2fb45ba247db367ba1d7b4f7560
jiabug <jiakangze@gmail.com>
2015-06-29 14:49:51 -0400

    finish multiply strings

--- practice/jiabug/Math/MultiplyStrings.java
+++ practice/jiabug/Math/MultiplyStrings.java
@@ -17,11 +17,43 @@ public class MultiplyStrings {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println(multiply("0","12"));
 
 	}
 	
     public static String multiply(String num1, String num2) {
+        if (num1 == null || num2 == null || num1.length() == 0 || num2.length() == 0) {
+        	return "";
+        }
         
+        if (num1.charAt(0) == '0') {//the most significant bit
+        	return "0";
+        }
+        if (num2.charAt(0) == '0') {
+        	return "0";
+        }
+        	
+        
+        StringBuilder res = new StringBuilder();
+        int num = 0;
+        
+        for (int i = num1.length() + num2.length(); i >=1; i--) {
+        	
+        	for (int j = Math.min(i-1, num1.length()); j>= 1; j--) {
+        		
+        		if(i - j <= num2.length()) {
+        			num += (int)(num1.charAt(j-1) - '0') * (int)(num2.charAt(i-1-j) - '0');
+        		}
+        		
+        		
+        	}
+        	if (i != 1 || num > 0) {
+    			res.append(num%10);
+    		}
+    		
+    		num /= 10;
+        }
+        return res.reverse().toString();
     }
 
 }
