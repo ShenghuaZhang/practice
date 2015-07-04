9b1d86c48fdbec0fd62e1db6bf6d10c245ab075c
kajia <kajia@CS1239-08.cs.stonybrook.edu>
2015-07-04 00:58:59 -0400

    begin valid sudoku

---
+++ practice/jiabug/HashTable/ValidSudoku.java
@@ -0,0 +1,25 @@
+package HashTable;
+
+/**
+ * 36
+ * https://leetcode.com/problems/valid-sudoku/
+ * 
+ * Determine if a Sudoku is valid, according to: Sudoku Puzzles - The Rules.
+ * The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+ * 
+ * @author kajia
+ *
+ */
+
+public class ValidSudoku {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+    public static boolean isValidSudoku(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+        	
+        }
+    }
+}
