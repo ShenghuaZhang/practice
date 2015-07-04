09198de7310d797c9a44276ca599e25ab7d3b38a
jiabug <jiakangze@gmail.com>
2015-07-04 02:08:19 -0400

    finish valid soduko (36, leetcode)

--- practice/jiabug/HashTable/ValidSudoku.java
+++ practice/jiabug/HashTable/ValidSudoku.java
@@ -1,5 +1,7 @@
 package HashTable;
 
+import java.util.HashSet;
+
 /**
  * 36
  * https://leetcode.com/problems/valid-sudoku/
@@ -19,7 +21,23 @@ public class ValidSudoku {
 	}
     public static boolean isValidSudoku(char[][] board) {
         for (int i = 0; i < 9; i++) {
+        	HashSet<Character> row = new HashSet<Character>();
+        	HashSet<Character> column = new HashSet<Character>();
+        	HashSet<Character> block = new HashSet<Character>();
         	
+        	for (int j = 0; j < 9; j++) {
+        		if (board[i][j] != '.' && !row.add(board[i][j]) ) {
+        			return false;
+        		}
+        		if (board[j][i] != '.' && !column.add(board[j][i])) {
+        			return false;
+        		}
+        		if (board[j/3 + (i/3)*3][j%3 + (i%3)*3] != '.' && !block.add(board[j/3 + (i/3)*3][j%3 + (i%3)*3])) {
+        			return false;
+        		}
+        		
+        	}
         }
+        return true;
     }
 }
