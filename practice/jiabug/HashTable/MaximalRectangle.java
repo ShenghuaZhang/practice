af8e862e641625a23c6e3ebbec6876b4ee769233
jiabug <jiakangze@gmail.com>
2015-07-10 22:41:40 -0400

    maximal rectangle

---
+++ practice/jiabug/HashTable/MaximalRectangle.java
@@ -0,0 +1,23 @@
+package HashTable;
+
+/**
+ * 85
+ * https://leetcode.com/problems/maximal-rectangle/
+ * 
+ * Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle containing all ones and return its area.
+ * 
+ * @author jiakangze
+ *
+ */
+
+public class MaximalRectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+    /*public static int maximalRectangle(char[][] matrix) {
+        
+    }*/
+
+}
