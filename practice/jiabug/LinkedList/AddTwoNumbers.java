3e851ca6af029320f162328958475802c516c7f5
jiabug <jiakangze@gmail.com>
2015-07-14 18:55:56 -0400

    finish add two numbers(2, leetcode)

--- practice/jiabug/LinkedList/AddTwoNumbers.java
+++ practice/jiabug/LinkedList/AddTwoNumbers.java
@@ -23,8 +23,39 @@ public class AddTwoNumbers {
 		// TODO Auto-generated method stub
 
 	}
-/*    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+        	return l2;
+        }
+        if (l2 == null) {
+        	return l1;
+        }
+        ListNode helper = new ListNode(0);
         
-    }*/
+        ListNode cur = helper;
+        int digit = 0;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+        	int x = (l1 == null? 0 : l1.val);
+        	int y = (l2 == null? 0 : l2.val);
+        	
+        	digit = x + y + carry;
+        	
+        	cur.next = new ListNode(digit%10);
+        	carry = digit / 10;
+        	cur = cur.next;
+        	
+        	if (l1 != null) {
+        		l1 = l1.next;
+        	}
+        	if (l2 != null) {
+        		l2 = l2.next;
+        	}
+        }
+        if (carry != 0) {
+        	cur.next = new ListNode(carry);
+        }
+        return helper.next;
+    }
 
 }
