aba65ba1838ce0709ed00ea3b8febb13ac099a47
jiabug <jiakangze@gmail.com>
2015-07-14 04:10:44 -0400

    finish linked list cycle (141, leetcode)

---
+++ practice/jiabug/LinkedList/LinkedListCycle.java
@@ -0,0 +1,39 @@
+package LinkedList;
+
+/**
+ * 141
+ * https://leetcode.com/problems/linked-list-cycle/
+ * 
+ * Given a linked list, determine if it has a cycle in it.
+ * 
+ * Follow up:
+ * Can you solve it without using extra space?
+ * 
+ * @author jiakangze
+ * 
+ * #LinkedListCycleII
+ * 
+ */
+
+public class LinkedListCycle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+    public static boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode runner = head;
+        while (runner.next != null && runner.next.next != null) {
+            runner = runner.next.next;
+            head = head.next;
+            if (runner == head) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
