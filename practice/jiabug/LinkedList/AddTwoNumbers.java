3100dbb20a7bc14d7bcb37408822dfa73f12af28
jiabug <jiakangze@gmail.com>
2015-07-14 22:31:59 -0400

    the recursive version of add two numbers

--- practice/jiabug/LinkedList/AddTwoNumbers.java
+++ practice/jiabug/LinkedList/AddTwoNumbers.java
@@ -57,5 +57,34 @@ public class AddTwoNumbers {
         }
         return helper.next;
     }
+    
+    public static ListNode addTwoNumbers_recursive(ListNode l1, ListNode l2) {
+    	return helper(l1,l2,0);
+    }
+    
+    private static ListNode helper(ListNode l1, ListNode l2, int carry) {
+    	if (l1 == null && l2 == null && carry == 0) {
+    		return null;
+    	}
+    	int value = carry;
+    	ListNode res = new ListNode(0);
+    	
+    	if (l1 != null) {
+    		value += l1.val;
+    	}
+    	if (l2 != null) {
+    		value += l2.val;
+    	}
+    	
+    	res.val = value % 10;
+    	
+    	if (l1 != null || l2 != null) {
+    		ListNode more = helper(l1 == null ? null : l1.next, 
+    				               l2 == null ? null : l2.next,
+    				                value/10);
+    		res.next = more;
+    	}
+    	return res;
+    }
 
 }
