3144935f698063f491ba3833fc6675cd7d2840a0
jiabug <jiakangze@gmail.com>
2015-07-17 18:24:29 -0400

    begin stack

---
+++ practice/jiabug/stack/ImplementQueueUsingStacks.java
@@ -0,0 +1,19 @@
+package stack;
+
+/**
+ * 232
+ * https://leetcode.com/problems/implement-queue-using-stacks/
+ * 
+ * 
+ * @author jiakangze
+ *
+ */
+
+public class ImplementQueueUsingStacks {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
