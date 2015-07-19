be007581e5e6126fa51bbb26b9cdcabef706d1ad
kajia <kajia@CS1239-03.cs.stonybrook.edu>
2015-07-19 00:35:27 -0400

    finish evaluate reverse polish notation (150, leetcode)

---
+++ practice/jiabug/stack/EvaluateReversePolishNotation.java
@@ -0,0 +1,55 @@
+package stack;
+
+import java.util.Stack;
+
+/**
+ * 150
+ * https://leetcode.com/problems/evaluate-reverse-polish-notation/
+ * 
+ * Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+ * Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+ * 
+ * Some examples:
+ *   ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+ *   ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
+ *   
+ * @author kajia
+ * yi's code
+ */
+
+public class EvaluateReversePolishNotation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(evalRPN(new String[]{"4","13","5","/","+"}));
+
+	}
+	
+    public static int evalRPN(String[] tokens) {
+        if (tokens == null || tokens.length == 0) {
+        	return 0;
+        }
+        
+        Stack<Integer> stack = new Stack<Integer>();
+        
+        for (int i = 0; i < tokens.length; i++) {
+        	String s = tokens[i];
+        	if (s.equals("+")) {
+        		stack.push(stack.pop() + stack.pop());
+        	} else if (s.equals("-")) {
+        		stack.push(-stack.pop() + stack.pop());
+        	} else if (s.equals("*")) {
+        		stack.push(stack.pop() * stack.pop());
+        	} else if (s.equals("/")) {
+        		//stack.push(1/stack.pop() * stack.pop()); if we use this, 5, 13, / will output 0; not 2
+        		int x = stack.pop();
+        		int y = stack.pop();
+        		stack.push(y / x);
+        	} else {
+        		int temp = Integer.parseInt(s);
+        		stack.push(temp);
+        	}
+        }
+        return stack.pop();
+    }
+}
