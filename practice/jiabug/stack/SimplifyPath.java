2d45b029924a001c4bf30b86cdc437cd7325193f
kajia <kajia@CS1239-03.cs.stonybrook.edu>
2015-07-19 01:36:32 -0400

    I will finish simplify path later!

---
+++ practice/jiabug/stack/SimplifyPath.java
@@ -0,0 +1,30 @@
+package stack;
+
+/**
+ * 71
+ * https://leetcode.com/problems/simplify-path/
+ * 
+ * Given an absolute path for a file (Unix-style), simplify it.
+ * 
+ * For example,
+ * 
+ * path = "/home/", => "/home"
+ * path = "/a/./b/../../c/", => "/c"
+ * 
+ * @author kajia
+ * http://blog.csdn.net/linhuanmars/article/details/23972563
+ */
+
+public class SimplifyPath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+    public static String simplifyPath(String path) {
+
+        return "hello";
+    }
+
+}
